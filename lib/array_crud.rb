7ce86c27cd056d337f74c921c3b3996818c9a69f
Timothy Tan <timtan93@gmail.com>
2018-11-21 19:47:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   [1,2,3,4]
 end
 
-def add_element_to_end_of_array(element)
+def add_element_to_end_of_array(array, element)
   array.push(element)
   
 end
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
-def remove_element_from_end_of_array(element)
+def remove_element_from_end_of_array(array)
   array.pop()
 end
 
